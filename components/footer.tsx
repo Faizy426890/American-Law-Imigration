3dfc85280405433b7109b6fc077ec93a30b515e7
Faizan Ahmed <faizanlahori2003@gmail.com>
2026-01-30 04:03:13 +0500

    footer updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -68,7 +68,6 @@ export default function Footer() {
             <ul className={styles.footerLinks}>
               <li><a href="/contact" className={styles.footerLink}>Schedule Consultation</a></li>
               <li><a href="/contact" className={styles.footerLink}>Contact Us</a></li>
-              <li><a href="/locations" className={styles.footerLink}>Office Locations</a></li>
               <li className={styles.contactItem}>
                 <svg className={styles.contactIcon} viewBox="0 0 24 24">
                   <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"></path>
